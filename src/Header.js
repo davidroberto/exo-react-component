d4c0e7b66d978ef2f3472356c5c338c5bea08027
David Robert <contact@david-robert.fr>
2023-11-07 12:15:30 +0100

    components

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <h1>Mon super blog</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/LatestArticles.js
@@ -0,0 +1,22 @@
+function LatestArticles() {
+  return (
+    <main>
+      <article>
+        <h2>Article XXXXX</h2>
+        <p>Hello article</p>
+      </article>
+
+      <article>
+        <h2>Article XXXXX</h2>
+        <p>Hello article</p>
+      </article>
+
+      <article>
+        <h2>Article XXXXX</h2>
+        <p>Hello article</p>
+      </article>
+    </main>
+  );
+}
+
+export default LatestArticles;
